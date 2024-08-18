1cd87e8b75c72f957b775c5837c65e412a86c31a
Euaell <euaelmeko@gmail.com>
2024-08-18 11:25:21 +0300

    setup

---
+++ src/components/contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact(): React.ReactNode {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>Email: your-email@example.com</p>
+    </section>
+  );
+}
+
+export default Contact;
